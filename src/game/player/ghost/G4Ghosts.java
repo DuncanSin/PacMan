785299eb91bc15ca5589cf3b75c3e99541f133f7
DuncanSin <DuncanSin@192.168.2.105>
2012-01-11 16:51:33 +0100

    Test

--- src/game/player/ghost/G4Ghosts.java
+++ src/game/player/ghost/G4Ghosts.java
@@ -10,10 +10,26 @@ public class G4Ghosts extends AbstractGhost {
 		int[] directions=new int[Game.NUM_GHOSTS];
 		int[] curPos=new int[Game.NUM_GHOSTS];
 		int[] curDir=new int[Game.NUM_GHOSTS];
+		int[] posDir=new int[4];
+		
+		double dist, temp;
 		
 		for(int i = 0; i<Game.NUM_GHOSTS; i++){
 			curPos[i] = game.getCurGhostLoc(i);
 			curDir[i] = game.getCurGhostDir(i);
+			
+			posDir = game.getGhostNeighbours(i);
+			
+			dist = 0;
+			
+			for(int j=0; j<4; j++){
+				temp = game.getEuclideanDistance(posDir[j], game.getCurPacManLoc());
+				
+				if(temp < dist){
+					dist = temp;
+					directions[i] = posDir[j];
+				}
+			}
 		}
 		
 		
