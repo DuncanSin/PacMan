db2049e25077cdff8fc21ef8986339484fd77c47
DuncanSin <DuncanSin@192.168.2.105>
2012-01-11 17:36:38 +0100

    Läuft nicht richtig

--- src/game/player/ghost/G4Ghosts.java
+++ src/game/player/ghost/G4Ghosts.java
@@ -7,34 +7,35 @@ public class G4Ghosts extends AbstractGhost {
 
 	@Override
 	public int[] getActions(Game game, long timeDue) {
-		int[] directions=new int[Game.NUM_GHOSTS];
-		int[] curPos=new int[Game.NUM_GHOSTS];
-		int[] curDir=new int[Game.NUM_GHOSTS];
-		int[] posDir=new int[4];
-		
+		int[] directions = new int[Game.NUM_GHOSTS];
+		int[] curPos = new int[Game.NUM_GHOSTS];
+		int[] curDir = new int[Game.NUM_GHOSTS];
+		int[] posDir = new int[4];
+
 		double dist, temp;
-		
-		for(int i = 0; i<Game.NUM_GHOSTS; i++){
-			curPos[i] = game.getCurGhostLoc(i);
-			curDir[i] = game.getCurGhostDir(i);
-			
-			posDir = game.getGhostNeighbours(i);
-			
-			dist = 0;
-			
-			for(int j=0; j<4; j++){
-				temp = game.getEuclideanDistance(posDir[j], game.getCurPacManLoc());
-				
-				if(temp < dist){
-					dist = temp;
-					directions[i] = posDir[j];
+
+		for (int i = 0; i < Game.NUM_GHOSTS; i++) {
+			if (game.ghostRequiresAction(i)) {
+				curPos[i] = game.getCurGhostLoc(i);
+				curDir[i] = game.getCurGhostDir(i);
+
+				posDir = game.getPossibleGhostDirs(i);
+
+				dist = 0;
+
+				for (int j = 0; j < posDir.length; j++) {
+					temp = game.getPathDistance(
+							game.getNeighbour(curPos[i], posDir[j]),
+							game.getCurPacManLoc());
+
+					if (temp <= dist) {
+						dist = temp;
+						directions[i] = posDir[j];
+					}
 				}
 			}
 		}
-		
-		
-		
-		
+
 		return directions;
 	}
 
