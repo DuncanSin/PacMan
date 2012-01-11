8042749bb21d7aa3f45ca6506fbf68b4a891e85a
DuncanSin <DuncanSin@192.168.2.105>
2012-01-11 16:18:47 +0100

    sy

---
+++ src/game/player/ghost/G4Ghosts.java
@@ -0,0 +1,20 @@
+package game.player.ghost;
+
+import game.core.Game;
+import gui.AbstractGhost;
+
+public class G4Ghosts extends AbstractGhost {
+
+	@Override
+	public int[] getActions(Game game, long timeDue) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getGhostGroupName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
