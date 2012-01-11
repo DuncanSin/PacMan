b1a3ae78e31c6658887ce283715d0f653ed39207
DuncanSin <DuncanSin@192.168.2.105>
2012-01-11 16:41:49 +0100

    Ghost Richtung und Ort

--- src/game/player/ghost/G4Ghosts.java
+++ src/game/player/ghost/G4Ghosts.java
@@ -8,7 +8,13 @@ public class G4Ghosts extends AbstractGhost {
 	@Override
 	public int[] getActions(Game game, long timeDue) {
 		int[] directions=new int[Game.NUM_GHOSTS];
+		int[] curPos=new int[Game.NUM_GHOSTS];
+		int[] curDir=new int[Game.NUM_GHOSTS];
 		
+		for(int i = 0; i<Game.NUM_GHOSTS; i++){
+			curPos[i] = game.getCurGhostLoc(i);
+			curDir[i] = game.getCurGhostDir(i);
+		}
 		
 		
 		
